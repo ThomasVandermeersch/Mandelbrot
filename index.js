7a5d87fd51eb9c37292985757757e427b087f6b4
ThomasVandermeersch <17030@ecam.be>
2021-11-16 15:44:13 +0100

    Initiliaze server

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+
+
+
+app.get('/', function (req, res) {
+    res.send('Hello World');
+})
+
+app.get('/mandelbrot',function(req,res){
+    res.send('Mandelbrot')
+})
+
+
+app.listen(8081)
+app.listen(8080)
